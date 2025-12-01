6b886971c71078394bcd47ba9666b40e132ca0de
“Jonas <joner.svensson@gmail.com>
2025-12-01 11:22:07 +0100

    Player setup complete (including bot)

---
+++ src/tictactoe/Game.java
@@ -0,0 +1,44 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class Game {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+
+    Scanner scanner = new Scanner(System.in);
+
+    // Game and settings
+    public Game(){
+
+        System.out.print("Enter Player 1's name: ");
+        String player1Name = scanner.nextLine();
+        char player1Marker = 'X';
+
+        this.player1 = new Player(player1Name, player1Marker, false);
+
+        System.out.print("Play vs bot or real player? (bot/player): ");
+        String playAgain = scanner.nextLine();
+
+        char player2Marker = 'O';
+
+        if (playAgain.equalsIgnoreCase("bot")) {
+
+            this.player2 = new Player("bot", player2Marker, true);
+        } else {
+                System.out.print("Enter Player 2's name: ");
+                String player2Name = scanner.nextLine();
+
+                this.player2 = new Player(player2Name, player2Marker, false);
+        }
+
+        this.currentPlayer = this.player1;
+        start();
+    }
+
+    // Game process
+    public void start(){
+        System.out.println("is this a bot? " + this.player2.getBot());
+    }
+}
--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -1,5 +1,27 @@
 package tictactoe;
 
 public class Player {
+    private String name;
+    private char marker;
+    private boolean bot;
+
+
+    public Player(String name, char marker, boolean bot){
+        this.name = name;
+        this.marker = marker;
+        this.bot = bot;
+    }
     
+    public String getName() {
+        return name;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+
+    public boolean getBot() {
+        return bot;
+    }
+
 }
