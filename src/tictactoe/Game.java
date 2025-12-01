3bea0e7e21db267217ccdb19edb9d9c62ddb78db
Joner <joner.svensson@gmail.com>
2025-12-01 13:00:22 +0100

    Added marker placement into the game (#3)
    Co-authored-by: “Jonas <“joner.svensson@gmail.com”>

--- src/tictactoe/Game.java
+++ src/tictactoe/Game.java
@@ -42,9 +42,45 @@ public class Game {
     public void start(){
         board = new Board();
 
-        // Debug
-        this.board.printBoard();
-        System.out.println("is this a bot? " + this.player2.getBot());
-        
+        while (true) {
+            this.board.printBoard();
+            
+            System.out.println(currentPlayer.getName() + "'s turn (" + currentPlayer.getMarker() + ")");
+            System.out.print("Enter column and row (e.g. 1B): ");
+
+            int col, row;
+
+            try{
+
+                String input = scanner.next().toUpperCase();
+
+                // Convert String input to ascii to get value between 0-2 (subtract ascii value)
+                col = ((int) (input.charAt(0)) - 49);
+                row = ((int) (input.charAt(1)) - 65);
+
+                // Initiates catch if col/row is not 0-2 after conversion
+                if (col < 0 || col > 2 || row < 0 || row > 2) {
+                    throw new IllegalArgumentException();
+                }
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a number between 1-3 for column and letter A, B or C for row.");
+                scanner.nextLine();
+                continue;
+            }
+
+            if (board.isCellEmpty(col, row)) {
+                board.placeMarker(col, row, currentPlayer.getMarker());
+            }
+
+            // Debug
+            System.out.println("is this a bot? " + this.player2.getBot());
+            System.out.println("row: " + row +" "+ "col: " + col);
+
+            switchPlayer();
+        }
+    }
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
 }
